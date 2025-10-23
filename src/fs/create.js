bfd88349a796ee8de8adeac224be7511c726b0b8
Konstantin <beeters88@gmail.com>
2025-10-23 23:17:16 +0300

    feat: add fs:create module

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,25 @@
+import fs from 'fs/promises';
+import path from 'path';
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const create = async () => {
-  // Write your code here
+  const folder = path.join(__dirname, 'files');
+  const filePath = path.join(folder, 'fresh.txt');
+  try {
+    await fs.access(filePath);
+    throw new Error('FS operation failed');
+  } catch(err) {
+    if (err.code === 'ENOENT') {
+      await fs.writeFile(filePath, 'I am fresh and young');
+    } else {
+      throw err;
+    }
+    
+  }
 };
 
 await create();
