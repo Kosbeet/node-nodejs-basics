17765550f954ceb179a697df5cd631c5ceb63676
Konstantin <beeters88@gmail.com>
2025-10-25 02:54:35 +0300

    feat: add cli:env task

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,10 @@
 const parseEnv = () => {
-  // Write your code here
+  const rssKeys = Object.keys(process.env).filter((key) => key.startsWith('RSS_'));
+  const rssVarsValues = rssKeys.map((key) => {
+    const value = process.env[key];
+    return `${key}=${value}`;
+  })
+  return rssVarsValues.join('; ');
 };
+console.log(parseEnv())
 
-parseEnv();
