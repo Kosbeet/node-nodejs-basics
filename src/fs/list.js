c753d3b35842e749fa29c06bde248281bafbd184
Konstantin <beeters88@gmail.com>
2025-10-24 20:21:26 +0300

    feat: add fs:list module

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,22 @@
+import fs from 'fs/promises';
+import path from 'path';
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+
 const list = async () => {
-  // Write your code here
+  const folder = path.join(__dirname, 'files');
+
+  try {
+    await fs.access(folder);
+    const files = await fs.readdir(folder);
+    console.log(files)
+  } catch(err) {
+    throw new Error('FS operation failed');
+  }
 };
 
 await list();
