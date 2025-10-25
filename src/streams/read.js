8a2a8c9435406a196cd521bcc783f7f915bc17ea
Konstantin <beeters88@gmail.com>
2025-10-26 00:47:18 +0300

    feat: add streams:read task

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,18 @@
+import fs from 'node:fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const read = async () => {
-  // Write your code here
-};
+  const toRead = path.join(__dirname, 'files', 'fileToRead.txt');
+
+  const readable = fs.createReadStream(toRead, { encoding: 'utf8'});
+  
+  readable.on('data', chunk => process.stdout.write(chunk));
+  readable.on('end', () => console.log(''))
+}
 
 await read();
