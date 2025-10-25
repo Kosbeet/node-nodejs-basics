b6a86476af903bc60fbf6c8c270518c37ed1a65d
Konstantin <beeters88@gmail.com>
2025-10-25 15:28:33 +0300

    feat: add cli:args task

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,13 @@
 const parseArgs = () => {
-  // Write your code here
+  const args = process.argv.slice(2).map((elem) => elem.replace('--', ''));
+  const result = [];
+
+  for (let i = 0; i < args.length; i += 2) {
+    const str = `${args[i]} is ${args[i + 1]}`
+    result.push(str);
+  }
+  // console.log(result.join(', '))
+  return result.join(', ');
 };
 
 parseArgs();
