6ff0ab850336f100f9751ac777602c4622da83c2
Konstantin <beeters88@gmail.com>
2025-10-26 01:17:25 +0300

    feat: add streams:write task

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,14 @@
+import fs from 'node:fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const write = async () => {
-  // Write your code here
+  const file = path.join(__dirname, 'files', 'fileToWrite.txt');
+  const writeable = fs.createWriteStream(file, { flags: 'a' });
+  process.stdin.pipe(writeable);
 };
 
 await write();
